e6f48811054a04bb87706c6216fdf19166222334
Eric Briand <eric.briand@gmail.com>
2021-02-25 20:26:39 +0100

    tayo!

--- solve.js
+++ solve.js
@@ -16,7 +16,7 @@ function solve({ parsedValue }, file) {
       index,
       streets: streets.streets.map(({ name }) => ({
         name,
-        seconds: streetsByJam.get(name)
+        seconds: Math.ceil(streetsByJam.get(name) / 2) || 1
       }))
     }))
   };
