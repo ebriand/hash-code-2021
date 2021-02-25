cb4c3664f388629b8bf6b5b28d7e1bb357aaf066
Eric Briand <eric.briand@gmail.com>
2021-02-25 19:16:26 +0100

    tayo!

--- solve.js
+++ solve.js
@@ -3,8 +3,43 @@ const _ = require("lodash");
 const gridUtils = require("./grid-utils");
 
 function solve(problem, file) {
-  // destructure this!
-  return [];
+  const result = {
+    numberOfIntersections: 3,
+    intersections: [
+      {
+        index: 1,
+        streets: [
+          {
+            name: "rue-d-athenes",
+            seconds: 2
+          },
+          {
+            name: "rue-d-amsterdam",
+            seconds: 1
+          }
+        ]
+      },
+      {
+        index: 0,
+        streets: [
+          {
+            name: "rue-de-londres",
+            seconds: 2
+          }
+        ]
+      },
+      {
+        index: 2,
+        streets: [
+          {
+            name: "rue-de-mouscou",
+            seconds: 1
+          }
+        ]
+      }
+    ]
+  };
+  return result;
 }
 
 module.exports = solve;
--- write.js
+++ write.js
@@ -1,5 +1,6 @@
 const _ = require("lodash/fp");
 const fs = require("fs");
+const { intersection } = require("lodash");
 const debug = require("debug")("write");
 
 module.exports = function write(path, solution) {
@@ -11,6 +12,17 @@ function writeLines(path, lines) {
   debug(`wrote ${lines.length} lines to ${path}`);
 }
 
-const unparse = _.constant(["0"]);
+const unparse = solution => {
+  const result = [];
+  result.push(solution.numberOfIntersections);
+  for (let intersection of solution.intersections) {
+    result.push(intersection.index);
+    result.push(intersection.streets.length);
+    for (let street of intersection.streets) {
+      result.push(street.name + " " + street.seconds);
+    }
+  }
+  return result;
+};
 
 module.exports.unparse = unparse;
